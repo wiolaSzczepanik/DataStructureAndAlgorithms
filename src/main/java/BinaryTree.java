539f3ce552237ee55e021031263a355ade36a22b
wiolaSzczepanik <wiolaszcz@gmail.com>
2018-05-17 12:35:58 +0200

    implement insert method and add Main class

--- src/main/java/BinaryTree.java
+++ src/main/java/BinaryTree.java
@@ -1,2 +1,45 @@
 public class BinaryTree {
-}
+    public static Node root;
+    public Node parent;
+
+    public BinaryTree() {
+        this.root = null;
+        this.parent = null;
+    }
+
+    public void insert(int id){
+        Node newNode = new Node(id);
+        if(root ==null){
+                root=newNode;
+                return;
+            }
+            Node current = root;
+            Node parent = null;
+            while(true){
+                parent = current;
+                if(id<current.data){
+                    current = current.left;
+                    if(current==null){
+                        parent.left= newNode;
+                        return;
+                    }
+                }else{
+                    current = current.right;
+                    if(current==null){
+                        parent.right = newNode;
+                        return;
+                    }
+                }
+            }
+        }
+
+        public void displayTree(Node root){
+            if(root!=null){
+                displayTree(root.left);
+                System.out.println(" "+root.data);
+                displayTree(root.right);
+            }
+        }
+    }
+
+
