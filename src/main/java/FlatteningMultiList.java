670667e0d32c9324b7e08347586c49ab5b0360b8
wiolaSzczepanik <wiolaszcz@gmail.com>
2018-05-22 23:28:17 +0200

    create Main class and remove main metod from FlatteningMultiList to Main class

--- src/main/java/FlatteningMultiList.java
+++ src/main/java/FlatteningMultiList.java
@@ -5,28 +5,19 @@ import static java.util.Arrays.asList;
 
 public class FlatteningMultiList {
 
-    public static List<Object> flattenMethod(List<?> nestedList){
-        List<Object> flatList=new LinkedList<Object>();
+    public List<Object> flattenMethod(List<?> nestedList) {
+        List<Object> flatList = new LinkedList<Object>();
 
-        for(Object element:nestedList){
-            if(element instanceof List<?>){
+        for (Object element : nestedList) {
+            if (element instanceof List<?>) {
                 List<Object> newList = flattenMethod((List<?>) element);
                 flatList.addAll(newList);
-            }else {
+            } else {
                 flatList.add(element);
             }
         }
         return flatList;
     }
-
-    public static void main(String[] args) {
-        List<Object> nestedList=lst(1, lst(2, lst(3,4)));
-        List<Object> flatList = flattenMethod(nestedList);
-        System.out.println(nestedList);
-        System.out.println(flatList);
-    }
-
-    private static List<Object> lst(Object... objects){
-        return asList(objects);
-    }
 }
+
+
